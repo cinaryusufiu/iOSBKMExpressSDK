4af76e9c0b826fc0cf115aeb5856006269db4dfd
YusufCinar <yusufcinar@alsay.com.tr>
2024-03-15 19:18:34 +0300

    Initial Commit

--- Package.swift
+++ Package.swift
@@ -14,8 +14,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "iOSBKMExpressSDK"),
         .binaryTarget(
             name: "iOSBKMExpressSDK",
             url: "URL_TO_YOUR_ZIP_FILE",

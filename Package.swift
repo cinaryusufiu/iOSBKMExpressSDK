ec929d3feda9c8d2128a214b615f68af45f824cd
YusufCinar <yusufcinar@alsay.com.tr>
2024-03-15 19:30:40 +0300

    Added checksum value

--- Package.swift
+++ Package.swift
@@ -6,18 +6,16 @@ import PackageDescription
 let package = Package(
     name: "iOSBKMExpressSDK",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "iOSBKMExpressSDK",
             targets: ["iOSBKMExpressSDK"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+       
         .binaryTarget(
             name: "iOSBKMExpressSDK",
-            url: "URL_TO_YOUR_ZIP_FILE",
-            checksum: "CHECKSUM_VALUE"
+            url: "https://github.com/cinaryusufiu/iOSBKMExpressSDK/releases/download/1.0.0/BKMExpressSDK.xcframework.zip",
+            checksum: "236d7fa559454c5f7c5f04d1c9b8d8eba41dd0bd47c4b06c73dddb4769954944"
         ),
         .testTarget(
             name: "iOSBKMExpressSDKTests",
